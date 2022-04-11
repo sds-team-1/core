58de8e98e4369d23669fe03d2009e200268c627a
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2022-04-11 00:13:07 -0400

    made copyservices to copy bash scripts to all active nodes

--- Files/CopyServices.sh
+++ Files/CopyServices.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 dir=/home/ubuntu/core/Files/Services/*
-
 sudo bash SetupCapturesFolder.sh
+cd /tmp/pycore.1/
+for dest in */; do cp -R $dir $dest ; done
 
-sudo cp -R $dir   /tmp/pycore.1/n11.conf
-sudo cp -R $dir   /tmp/pycore.1/n12.conf
