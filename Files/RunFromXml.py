5e4bad0fa398ddabdb7c71a09920f8833d64903e
ubuntu <erikrivera40@gmail.com>
2022-04-12 18:48:49 -0600

    update with core repo master

---
+++ Files/RunFromXml.py
@@ -0,0 +1,69 @@
+
+from core.emulator.coreemu import CoreEmu
+from pathlib import Path
+from core.nodes.base import CoreNode
+from core.emulator.data import IpPrefixes, NodeOptions
+from core.emulator.enumerations import EventTypes
+import sys
+import logging
+import os
+
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(stream = sys.stdout, 
+                    filemode = "w",
+                    format = Log_Format, 
+                    level = logging.INFO)
+
+logger = logging.getLogger(__name__)
+path = Path("/home/ubuntu/core/Files/new.xml")
+
+coreemu = CoreEmu()
+session = coreemu.create_session()
+
+# must be in configuration state for nodes to start, when using "node_add" below
+session.open_xml(path, start=True)
+session.instantiate()
+
+
+# do whatever you like here
+input("press enter to shutdown")
+
+# stop session
+session.shutdown()
+
+# # required imports
+# from core.emulator.coreemu import CoreEmu
+# from core.emulator.data import IpPrefixes, NodeOptions
+# from core.emulator.enumerations import EventTypes
+# from core.nodes.base import CoreNode
+
+# # ip nerator for example
+# ip_prefixes = IpPrefixes(ip4_prefix="10.0.0.0/24")
+
+# # create emulator instance for creating sessions and utility methods
+# coreemu = CoreEmu()
+# session = coreemu.create_session()
+
+# # must be in configuration state for nodes to start, when using "node_add" below
+# session.set_state(EventTypes.CONFIGURATION_STATE)
+
+# # create nodes
+# options = NodeOptions(x=100, y=100)
+# n1 = session.add_node(CoreNode, options=options)
+# options = NodeOptions(x=300, y=100)
+# n2 = session.add_node(CoreNode, options=options)
+
+# # link nodes together
+# iface1 = ip_prefixes.create_iface(n1)
+# iface2 = ip_prefixes.create_iface(n2)
+# session.add_link(n1.id, n2.id, iface1, iface2)
+
+# # start session
+# session.instantiate()
+
+# # do whatever you like here
+# input("press enter to shutdown")
+
+# # stop session
+# session.shutdown()
