86ceb5c873a813f43d1f5da800fdefbf424933e6
ubuntu <erikrivera40@gmail.com>
2022-04-14 22:57:48 -0600

    update creation of pcaps folder

--- Files/SetupCapturesFolder.sh
+++ Files/SetupCapturesFolder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ -d "/home/ubuntu/core/Files/Captures" ]; then
+if [ -d "/home/ubuntu/core/Files/pcaps" ]; then
   # Control will enter here if $DIRECTORY exists.
-  echo "Clearing Captures Folder..."
-  rm -r "/home/ubuntu/core/Files/Captures/"
-  mkdir Captures
+  echo "Clearing pcaps Folder..."
+  rm -r "/home/ubuntu/core/Files/pcaps/"
+  mkdir "/home/ubuntu/core/Files/pcaps/"
 else
-    echo "Creating Captures Folder"
-    mkdir Captures
+    echo "Creating pcaps Folder"
+    mkdir "/home/ubuntu/core/Files/pcaps/"
 fi
