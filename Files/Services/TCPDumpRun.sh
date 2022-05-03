ea2b13465e7423a288aa93891e5526cdbe745b57
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2022-05-03 06:34:04 -0400

    new TCP duump pringts pcap with node name

--- Files/Services/TCPDumpRun.sh
+++ Files/Services/TCPDumpRun.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-v=$(cat TCPArgs.txt)
-if [ -z "$v" ]
-then 
-    echo "No argument TCPDump"
-else
-    sudo tcpdump -s 0 -w ~/core/Files/pcaps/$v 
-fi
+#v=$(cat TCPArgs.txt)
+#if [ -z "$v" ]
+#then 
+#    echo "No argument TCPDump"
+#else
+#    sudo tcpdump -s 0 -w ~/core/Files/pcaps/$v 
+#fi
+
+
+
 #sudo tcpdump -s 0 -w ~/core/Files/pcaps/pcap1.pcap
 
-
+result=${PWD##*/} 
+sudo tcpdump -s 0 -w /home/ubuntu/core/Files/pcaps/${result//.conf/}.pcap 
