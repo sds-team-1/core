62c21f6063a42449ed7ba2ed7ef3bcfa8541e542
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2022-05-03 09:47:44 -0400

    asdf

--- Files/CopyServices.sh
+++ Files/CopyServices.sh
@@ -3,4 +3,6 @@ dir=/home/ubuntu/core/Files/Services/*
 sudo bash /home/ubuntu/core/Files/SetupCapturesFolder.sh
 cd /tmp/pycore.1/
 for dest in */; do cp -R $dir $dest ; done
+cd /home/ubuntu/core/Files/
+sudo bash StartServices.sh
 
