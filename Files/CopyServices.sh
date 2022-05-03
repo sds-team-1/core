e3a50c2360538a2334bd419961322bda7c5fde77
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2022-05-03 09:27:20 -0400

    added absolute path for setupcaturesfolder.sh

--- Files/CopyServices.sh
+++ Files/CopyServices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir=/home/ubuntu/core/Files/Services/*
-sudo bash SetupCapturesFolder.sh
+sudo bash /home/ubuntu/core/Files/SetupCapturesFolder.sh
 cd /tmp/pycore.1/
 for dest in */; do cp -R $dir $dest ; done
 
