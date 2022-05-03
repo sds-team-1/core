9742fc645360c2f1b872887b6ffe00f82de07132
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2022-05-03 07:19:04 -0400

    runs tcpdump on all nodes

--- Files/StartServices.sh
+++ Files/StartServices.sh
@@ -8,15 +8,20 @@
 # 3 ip of tcp node
 
 
-sudo bash /home/ubuntu/core/Files/CopyServices.sh
+#sudo bash /home/ubuntu/core/Files/CopyServices.sh
 
-sudo bash /home/ubuntu/core/Files/RunTCPDump.sh $1 $1.pcap &
-sudo bash /home/ubuntu/core/Files/RunTCPDump.sh $2 $2.pcap &
-sudo bash /home/ubuntu/core/Files/RunWebServer.sh $2 &
+#sudo bash /home/ubuntu/core/Files/RunTCPDump.sh $1 $1.pcap &
+#sudo bash /home/ubuntu/core/Files/RunTCPDump.sh $2 $2.pcap &
+#sudo bash /home/ubuntu/core/Files/RunWebServer.sh $2 &
 
 # --system-dns 10.0.1.20
 
-sudo bash /home/ubuntu/core/Files/RunNmap.sh $2 "--system-dns $3" &
-sudo bash /home/ubuntu/core/Files/RunNikto.sh $2 "-h $3 -p 8000"
+#sudo bash /home/ubuntu/core/Files/RunNmap.sh $2 "--system-dns $3" &
+#sudo bash /home/ubuntu/core/Files/RunNikto.sh $2 "-h $3 -p 8000"
+
+
+
+cd /tmp/pycore.1/
+for dest in */; do vcmd -c /tmp/pycore.1/${dest//.conf\//} bash TCPDumpRun.sh & done
 
 
